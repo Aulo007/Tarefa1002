a9f8e2366f8521744f953221f6a678cb2ef8daff
Aulo Cezar <Aulofferreira@gmail.com>
2025-02-16 17:31:59 -0300

    Update main.c

--- main.c
+++ main.c
@@ -28,14 +28,39 @@ static const uint32_t I2C_SCL = 15;
 #define SQUARE_SIZE 8
 #define I2C_ADDR 0x3C
 
+// Variáveis globais
+static ssd1306_t display;
+static volatile bool led_green_state = false;
+static volatile bool pwm_enabled = true;
+static volatile uint8_t border_style = 0;
+static volatile uint32_t last_button_time = 0;
+const uint32_t DEBOUNCE_DELAY = 200000; // 200ms em microssegundos
 
+void setup_pwm(uint gpio); // Função para configurar pinos com pwm
 
 int main(void)
 {
     // Inicialização e lógica do programa
 
-
-
-
     return 0;
+}
+
+// Configuração do PWM para o LED
+void setup_pwm(uint gpio)
+{
+    gpio_set_function(gpio, GPIO_FUNC_PWM);
+    uint slice = pwm_gpio_to_slice_num(gpio);
+    pwm_config config = pwm_get_default_config();
+    pwm_config_set_clkdiv(&config, 1.0f); // Define o divisor de clock para evitar flickering
+    pwm_config_set_wrap(&config, 4095);   // Mesma resolução do ADC
+    pwm_init(slice, &config, true);
+}
+
+// Configura o valor PWM para um LED
+void set_pwm(uint gpio, uint16_t value)
+{
+    if (pwm_enabled)
+        pwm_set_gpio_level(gpio, value);
+    else
+        pwm_set_gpio_level(gpio, 0);
 }
